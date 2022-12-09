95a3b4693025839df4e9243558ace23acb1b230f
Jacob Bisher <jmb10101@gmail.com>
2022-12-08 19:41:35 -0600

    Completed Day 1.

---
+++ Day1/Program.cs
@@ -0,0 +1,20 @@
+﻿// AdventOfCode2022-Day1
+
+var lines = File.ReadAllLines("input.txt");
+
+var elfCalories = new List<int>();
+elfCalories.Add(0);
+foreach (var line in lines)
+{
+    if (string.IsNullOrEmpty(line))
+    {
+        elfCalories.Add(0);
+    }
+    else
+    {
+        elfCalories[^1] += int.Parse(line);
+    }
+}
+
+var top3Sum = elfCalories.OrderByDescending(x => x).Take(3).Sum();
+Console.WriteLine($"Max calories:{elfCalories.Max()} Top 3 sum:{top3Sum}");
