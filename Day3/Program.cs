dc10dc446a28a5929da19f84d8fe782760e00b9a
Jacob Bisher <jmb10101@gmail.com>
2022-12-09 18:51:34 -0600

    Started Day 3.

---
+++ Day3/Program.cs
@@ -0,0 +1,15 @@
+﻿// AdventOfCode2022-Day3
+
+namespace Day3
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt");
+
+            
+            Console.WriteLine($"{lines.Length}");
+        }
+    }
+}
