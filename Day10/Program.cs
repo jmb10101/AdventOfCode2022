f5a16fae60b8e9ee9d9d7ad47695f0023e4b0497
Jacob Bisher <jmb10101@gmail.com>
2022-12-10 17:12:35 -0600

    Started Day 10.

---
+++ Day10/Program.cs
@@ -0,0 +1,18 @@
+﻿// AdventOfCode2022-Day10
+
+namespace Day10
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt");
+            foreach (var line in lines)
+            {
+                
+            }
+            
+            Console.WriteLine($"1:");
+        }
+    }
+}
---
+++ Day11/Program.cs
@@ -0,0 +1,18 @@
+﻿// AdventOfCode2022-Day11
+
+namespace Day11
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt");
+            foreach (var line in lines)
+            {
+                
+            }
+            
+            Console.WriteLine($"1:");
+        }
+    }
+}
